827dace1a50a7d48f662fabe3cdb1edbc0ff004c
hasangad <mail@hasangad.com>
2017-09-17 21:02:44 +0300

    kkk

--- www/js/index.js
+++ www/js/index.js
@@ -41,7 +41,7 @@ var app = {
         console.log('calling push init');
         var push = PushNotification.init({
             "android": {
-                "senderID": "XXXXXXXX"
+                "senderID": "499005818743"
             },
             "browser": {},
             "ios": {
