bf47d8389f2c4eab82dd50879dc200413d9cde31
hasangad <hasan.gadallah@gmail.com>
2017-09-18 12:44:06 +0300

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -33,7 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-alert("Device IS ready");
+//alert("Device IS ready");
 
 
         console.log('Received Device Ready Event');
