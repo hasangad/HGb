4b7ec40981e8396f7cfb0bbf23490247b5a66f63
Hasan Gad Allah <hasan.gadallah@gmail.com>
2017-11-18 11:08:12 +0300

    lll

--- www/js/index.js
+++ www/js/index.js
@@ -28,9 +28,9 @@ var app = {
    
     
     
-  /*  bindEvents: function() {
+   bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-    },*/
+    },
         
         
         
@@ -39,7 +39,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-alert("Device IS ready");
+//alert("Device IS ready");
 //alert( 'We are Testing ' );
        
         
