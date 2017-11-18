6abcd3cc3200f22f1ee6991f566b64f8be7dd83f
Hasan Gad Allah <hasan.gadallah@gmail.com>
2017-11-18 11:13:45 +0300

    ccc

--- www/js/index.js
+++ www/js/index.js
@@ -18,8 +18,8 @@
  */
 var app = {
     // Application Constructor
-    //initialize: function() {
-     //   this.bindEvents();
+    initialize: function() {
+    this.bindEvents();
     },
     // Bind Event Listeners
     //
@@ -43,8 +43,8 @@ var app = {
 //alert( 'We are Testing ' );
        
         
-      //  console.log('Received Device Ready Event');
-      //  console.log('calling setup push');
+      console.log('Received Device Ready Event');
+      console.log('calling setup push');
        
         
         //app.setupPush();
